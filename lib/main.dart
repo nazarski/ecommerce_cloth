42c852aca1b0a0b301a60fcbec8054a89420a5b0
nazarski <nazar.volitskii@gmail.com>
2023-02-16 11:20:31 +0200

    Created firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
+import 'package:ecommerce_cloth/firebase_options.dart';
+import 'package:ecommerce_cloth/presentation/pages/splash_screen/splash_screen.dart';
 import 'package:ecommerce_cloth/resources/app_themes.dart';
-import 'package:ecommerce_cloth/presentation/pages/main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       themeMode: ThemeMode.system,
       theme: AppThemes.lightTheme,
       darkTheme: AppThemes.darkTheme,
-      home: MainPage(),
+      home: SplashScreen(),
     ),
   );
 }
