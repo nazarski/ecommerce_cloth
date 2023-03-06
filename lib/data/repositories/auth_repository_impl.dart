1ee35f516b86671a3c0e534df3bc1199da65d178
vladyslavtrachuk <vladyslavtrachuk@gmail.com>
2023-03-06 15:58:27 +0100

    [Authenticate] Initial authorization settings without architectural improvements

--- lib/data/repositories/auth_repository_impl.dart
+++ lib/data/repositories/auth_repository_impl.dart
@@ -1,5 +1,15 @@
+import 'package:ecommerce_cloth/data/data_sources/remote/authenticate_data.dart';
 import 'package:ecommerce_cloth/domain/repositories/auth_repository.dart';
 
-class AuthRepositoryImpl implements AuthRepository{
-
+class AuthRepositoryImpl implements AuthRepository {
+  @override
+  Future<int?> requestUserRegistration(
+      {required String email, required String username, required String password}) async {
+    final int? postRequestRegistrationUser = await AuthenticateData.postUserRegistration(
+      email: email,
+      username: username,
+      password: password,
+    );
+    return postRequestRegistrationUser;
+  }
 }
