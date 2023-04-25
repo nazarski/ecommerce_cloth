4dcfe165665d128707e2205d7c99f9d014b7d4ef
TrachukV <107151652+TrachukV@users.noreply.github.com>
2023-04-25 13:48:17 +0200

    [Logic] fix

---
+++ lib/data/data_sources/remote/addresses_remote_data.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:ecommerce_cloth/data/data_sources/remote/strapi_initialize.dart';
+import 'package:ecommerce_cloth/data/models/user_model/user_address_model.dart';
+
+class AddressesRemoteData{
+  AddressesRemoteData._();
+  static final _dio = Dio();
+  static const _apiEndpoint = StrapiInitialize.apiEndpoint;
+
+
+  static Future<List<UserAddressModel>> getUserAddresses({required String userId}) async {
+    final response = await _dio.get(
+        '$_apiEndpoint/users/$userId?populate=addresses');
+    final addressesResponse = List<Map<String, dynamic>>.from(response.data['addresses']);
+    final result = addressesResponse.map((element) {
+      return UserAddressModel.fromMap(element);
+    }).toList();
+    return result;
+  }
+}
